59b7584fd427b821fac77b94a6bc57038b41af07
Aaron Isaac <aaron@aisaac.co>
2023-01-29 15:34:49 -0600

    update

--- main.c
+++ main.c
@@ -5,36 +5,34 @@
 #define STEP 20
 
 void fahrenheit() {
-    for(int fahr = LOWER; fahr <= UPPER; fahr += STEP){
-        printf("%3d\t%6.1f\n", fahr, (5.0/9.0) * (fahr-32.0));
+    for (int fahr = LOWER; fahr <= UPPER; fahr += STEP) {
+        printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
     }
 }
 
-void copy(){
+void copy() {
     int c;
     printf("%d\n", EOF);
-    while((c = getchar()) != EOF)
+    while ((c = getchar()) != EOF)
         putchar(c);
 }
 
-void count(){
+void count() {
     long long nc;
-    for(nc = 0; getchar() != EOF; ++nc)
-        ;
+    for (nc = 0; getchar() != EOF; ++nc);
     printf("%lld\n", nc);
 }
 
-void count_chars(){
+void count_chars() {
     long c;
-    for(c = 0; getchar() != '\n'; ++c)
-        ;
+    for (c = 0; getchar() != '\n'; ++c);
     printf("%ld\n", c);
 }
 
-void count_lines(){
+void count_lines() {
     long d = 0;
-    for(int c = 0; (c = getchar()) != EOF;){
-        if(c == '\n') d++;
+    for (int c = 0; (c = getchar()) != EOF;) {
+        if (c == '\n') d++;
     }
     printf("%ld\n", d);
 }
